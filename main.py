ef8d3e2c67c9af53ceda2190ea290ed59a174d12
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-13 14:56:28 +0200

    refactored snake movement logic

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from turtle import Turtle, Screen
+import time
 
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+# Tracer is off so screen only gets updated when we need it to:
+screen.tracer(0)
 
 
 def create_snake():
@@ -27,7 +30,12 @@ game_is_on = True
 segments = create_snake()
 
 while game_is_on:
-    for seg in segments:
-        seg.forward(20)
+    screen.update()
+    time.sleep(0.1)
+    # Starting from the last segment (back of the snake), move each segment to the position of the segment in front of it (like a real worm would move)
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
 
 screen.exitonclick()
