6c9c7f71400f28f2de625178df41ad3d2da68cdc
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-13 14:08:41 +0200

    moved snake creation logic inside function

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
-# Starting pos for each of the 3 squares that the snake consists of
-starting_position = [0, 0]
 
-# Creates & places the 3 squares that the snake consists of
-for _ in range(3):
-    turtle = Turtle(shape='square')
-    turtle.color('white')
-    turtle.setposition(tuple(starting_position))
-    starting_position[0] -= 20
+def create_snake():
+    '''Creates snake avatar'''
+    # Starting pos for each of the 3 squares that the snake consists of
+    starting_position = [0, 0]
+
+    # Creates & places the 3 squares that the snake consists of
+    for _ in range(3):
+        turtle = Turtle(shape='square')
+        turtle.color('white')
+        turtle.setposition(tuple(starting_position))
+        starting_position[0] -= 20
 
 
 screen.exitonclick()
