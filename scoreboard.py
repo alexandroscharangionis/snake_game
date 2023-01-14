62edc169fc55527dc495160caaf83b4848bac550
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-14 15:51:40 +0200

    created game_over method

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,8 @@
 from turtle import Turtle
 
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
 
 class Scoreboard(Turtle):
     def __init__(self):
@@ -12,10 +15,15 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center",
-                   font=('Courier', 24, 'normal'))
+        self.write(f"Score: {self.score}", align=ALIGNMENT,
+                   font=FONT)
 
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align=ALIGNMENT,
+                   font=FONT)
